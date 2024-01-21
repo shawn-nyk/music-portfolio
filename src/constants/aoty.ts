03ed59879634e991bac4a26f1446dbe58cfab5c2
shawn-nyk <shawnngyongkwang@gmail.com>
2024-01-22 01:24:29 +0800

    Wrap OTY grid/list

--- src/constants/aoty.ts
+++ src/constants/aoty.ts
@@ -1,6 +1,28 @@
 const aoty2023 = [
-  ["CEP - Drawing The Target Around The Arrow", "1Uu3fza2uwuAM9kbn5EtBs"],
-  ["1969 Collective - 1969", "2NEtbWNxBAfOjMf0WPV38q"],
+  ["Anthony Naples - orbs", "2diDpno1K39g7HtGkPWiTe"],
+  ["John Carroll Kirby - Blowout", "1PtMyRufUsbQ81yMxXjSym"],
+  ["Khotin - Release Spirit", "3Grw8AYCWpxN7RpOvrtxjF"],
+  ["Euglossine - Bug Planet Is The Current Timeline", "177o5J2HA5YBotifiOZrin"],
+  ["Wilco - Cousin", "1KZy1Oll0Cnnakyy6X92Wn"],
+  ["George Clanton - Ooh Rap I Ya", "2kvz9frUn3LUK83imD2BQS"],
+  ["Mort Garson - Journey To The Moon And Beyond", "6xaioaJ2ndCnEK8Zz5uOD7"],
+  ["Kali Uchis - Red Moon In Venus", "5OZ44LaqZbpP3m9B3oT8br"],
+  ["Penguin Cafe - Rain Before Seven...", "4gGmqjw64211B9R3p2ikne"],
+  ["Hanakiv - Goodbyes", "5En0oSbOCytMKIlTKa8UUE"],
+  ["Foo Fighters - But Here We Are", "4wp4aWWpoYfNcspimVAnel"],
+  ["Nathan Fake - Crystal Vision", "3CL1XvaBaoVyMWpBUfnWtj"],
+  ["Oneohtrix Point Never - Again", "2zz6j2mGH5JHk0ihNvy6KM"],
+  ["PinkPantheress - Heaven knows", "2pOEFqvfxp5uUQ8vQEmVA0"],
+  ["Tinashe - BB/ANG3L", "0Ytzeqoskd5VfSVfGGA1SM"],
+  ["100 gecs - 10,000 gecs", "2XS5McKf3zdJWpcZ4OkZPZ"],
+  [
+    "James Holden - Imagine This Is A High Dimensional Space of All Possibilities",
+    "4c04wXoadgnqqrxeDUCnFP",
+  ],
+  [
+    "Caroline Polachek - Desire, I Want To Turn Into You",
+    "22PkV1Le9P3X4RY4xtmK0q",
+  ],
 ];
 
 const aoty2022 = [
@@ -8,7 +30,13 @@ const aoty2022 = [
   ["CEP - Drawing The Target Around The Arrow", "1Uu3fza2uwuAM9kbn5EtBs"],
 ];
 
+const aoty2021 = [
+  ["CEP - Drawing The Target Around The Arrow", "1Uu3fza2uwuAM9kbn5EtBs"],
+  ["1969 Collective - 1969", "2NEtbWNxBAfOjMf0WPV38q"],
+];
+
 export const AOTYS = [
   ["2023", aoty2023],
   ["2022", aoty2022],
+  ["2021", aoty2021],
 ];
